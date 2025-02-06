0d5cb7023fe1c62692ed4c011626b15af9114e57
Kun Yuan <31314392+Flaick@users.noreply.github.com>
2025-02-06 13:20:20 +0100

    Update __init__.py

--- surgvlp/codes/models/algorithms/__init__.py
+++ surgvlp/codes/models/algorithms/__init__.py
@@ -4,3 +4,5 @@ Project: Learning Multi-modal Representations by Watching Hundreds of Surgical V
 Copyright (c) University of Strasbourg, All Rights Reserved.
 """
 from .SurgVLP import *
+from .HecVL import *
+from .PeskaVLP import *
